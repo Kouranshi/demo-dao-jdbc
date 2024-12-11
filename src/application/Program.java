063dca32299e0a649c4d50a94aac93edd9b1d9d1
Kouranshi <ailey.dev@gmail.com>
2024-12-11 14:19:57 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller insert =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 
 	}
 
